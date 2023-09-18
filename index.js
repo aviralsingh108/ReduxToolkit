69de81cb391b127019f877863b8f6e1718067c70
aviralsingh108 <admiralaviral@gmail.com>
2023-09-18 19:26:53 +0530

    Creating Redux Store

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const CAKE_ORDERED = "CAKE_ORDERED";
 
 function orderCake() {
   // Action Creator is a function that create or returns an Object
-  return;
-  {
+  return {
     // Action
-    type: CAKE_ORDERED;
-    quantity: 1;
-  } // Action is an object with type property
+    type: CAKE_ORDERED,
+    quantity: 1,
+  }; // Action is an object with type property
 }
 
 // This is State, since the state should be an Object as per the first of the three main Redux Principle
@@ -28,3 +30,16 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+// Redux Store is now holding the application state
+const store = createStore(reducer);
+console.log("initial state", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+unsubscribe();
+// No Listener gets called once it is unsubscribed
+store.dispatch(orderCake());
