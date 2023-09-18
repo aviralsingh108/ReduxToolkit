f21c6e1222ebe68c3389d8cc95eb42d0a0cb3861
aviralsingh108 <admiralaviral@gmail.com>
2023-09-18 16:17:53 +0530

    added State and Reducer

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ function orderCake() {
     quantity: 1;
   } // Action is an object with type property
 }
+
+// This is State, since the state should be an Object as per the first of the three main Redux Principle
+const initialState = {
+  numOfCakes: 10,
+};
+
+// This is Reducer, it takes up the previous state and action, then returns a new State post updating it using
+// the action
+// (previousState, action) => newState;
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
