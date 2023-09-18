2e9f4a3a0b636ca7f4bb1556f4a2cd1e704eaf4d
aviralsingh108 <admiralaviral@gmail.com>
2023-09-18 16:02:25 +0530

    Action and Action Creators

---
+++ index.js
@@ -0,0 +1,11 @@
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+function orderCake() {
+  // Action Creator is a function that create or returns an Object
+  return;
+  {
+    // Action
+    type: CAKE_ORDERED;
+    quantity: 1;
+  } // Action is an object with type property
+}
